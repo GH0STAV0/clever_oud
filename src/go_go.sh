89d013c4e3dc456986ff6576f1e5848f572c70dc
GH0STAV0 <you@example.com>
2022-04-17 16:23:59 -0400

    my connecto python

---
+++ src/go_go.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+clear
+ngrok authtoken $NGROK_TOKENS
+ngrok http 6901 > /dev/null &
+export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")" && echo $WEBHOOK_URL
+trap "echo oh;exit" SIGTERM SIGINT
+echo $google_main > /root/g00g
+# echo -e "nameserver 103.86.96.100\nnameserver 103.86.99.100" >  /etc/resolv.conf
+echo "nameserver 8.8.8.8" >  /etc/resolv.conf
+echo "nameserver 8.8.4.4" >>  /etc/resolv.conf
+
+cd /headless/SDA_ALL/
+rm -rf site_5
+git reset --hard
+git pull
+cd /headless/SDA_ALL/main_oct0pus/
+chmod +x *
+pwd
+while true
+do
+	echo "NEW ..............."
+	dbus-uuidgen > /var/lib/dbus/machine-id
+	cd /headless/SDA_ALL/main_oct0pus/
+	# timeout 5m python3 48_ads.py
+	python3 google_let.py
+done
